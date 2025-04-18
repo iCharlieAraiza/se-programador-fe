fad45b0d529ac246ed4a2718438d61a82cb6d6ed
Carlos Araiza <eliptikman1234@gmail.com>
2025-04-17 22:40:08 -0600

    Add course topics and update course content for "Proceso de contratación"

--- constants/data.ts
+++ constants/data.ts
@@ -1,15 +1,56 @@
-export interface CoursesType {
+export interface TopicType {
     title: string;
     slug: string;
     number: number;
 }
 
+export interface CoursesType {
+    title: string;
+    slug: string;
+    number: number;
+    topics?: TopicType[];
+}
 
-export const COURSES : CoursesType[]  = [
+
+
+
+export const COURSES: CoursesType[] = [
     {
-        title: 'Proceso de Contratación - Entrevista Primer Contacto',
+        title: 'Proceso de contratación - Primer contacto y entrevista',
         slug: 'proceso-contratacion-entrevista-primer-contacto',
         number: 1,
+        topics: [
+            {
+                title: "Cómo es la primer toma de contacto",
+                slug: "como-es-la-primer-toma-de-contacto",
+                number: 1
+            },
+            {
+                title: "Preguntas frecuentes con reclutadores",
+                slug: "preguntas-frecuentes-con-reclutadores",
+                number: 2
+            },
+            {
+                title: "Errores comunes durante la primer entrevista",
+                slug: "errores-comunes-durante-la-primer-entrevista",
+                number: 3
+            },
+            {
+                title: "¿Qué es lo que se piden para desarrollador web (front-end y Full Stack)?",
+                slug: "que-es-lo-que-se-piden-para-desarrollador-web-front-end-y-full-stack",
+                number: 4
+            },
+            {
+                title: "Qué es lo que se pide para desarrollador Back End",
+                slug: "que-es-lo-que-se-pide-para-desarrollador-back-end",
+                number: 5
+            },
+            {
+                title: "Qué es lo que se pide para desarrollador Movil",
+                slug: "que-es-lo-que-se-pide-para-desarrollador-movil",
+                number: 6
+            }
+        ]
     },
     {
         title: 'Pruebas Técnicas - Live Coding',
