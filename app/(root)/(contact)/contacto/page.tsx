28aa0cba375f1977cc458a73afa6dc50def38aac
Carlos Araiza <eliptikman1234@gmail.com>
2025-05-01 12:23:14 -0600

    Fix contact page bug

--- app/(root)/(contact)/contacto/page.tsx
+++ app/(root)/(contact)/contacto/page.tsx
@@ -1,14 +1,10 @@
 import React from 'react'
 
-export default function RootLayout({
-    children,
-}: Readonly<{
-    children: React.ReactNode;
-}>) {
+export default function ContactPage() {
     return (
         <main>
             <article className='max-w-3xl mx-auto px-4 py-4 md:py-10'>
-                This is a contact page
+                    Página de contacto en construcción
                 </article>
         </main>
     );
